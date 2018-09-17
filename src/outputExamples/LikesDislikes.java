7b61d071ce8c4da91245792a0097e670245147e5
Matt Zepecki <mattzepecki21@gmail.com>
2018-09-17 09:14:10 -0500

    Likes and Dislikes

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes     Dislikes");	
+		System.out.println("Fottball  Tomatoes");
+		System.out.println("Music     Books");
+		System.out.println("Food      Cricket");
+		System.out.println("Dogs      Bugs");
+		System.out.println("Movies    Being patient");
+	}
+
+}
