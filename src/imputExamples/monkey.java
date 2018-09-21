69c2911b095ccc58e5d0ec391b2b08d777e6e336
Matt Zepecki <mattzepecki21@gmail.com>
2018-09-21 09:09:25 -0500

    Monkey height

---
+++ src/imputExamples/monkey.java
@@ -0,0 +1,14 @@
+package imputExamples;
+import java.util.Scanner;
+
+public class monkey {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int height=10;
+Scanner imput=new Scanner(System.in);
+System.out.println("What is the height of an adult monkey?");
+height=imput.nextInt();
+System.out.println("Height of an adult money is" + height);
+	}
+
+}
